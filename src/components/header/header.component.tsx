db71e4905cba1f98fac0647ea520099b5515e821
Vitor Souza <vitor.souza407@yahoo.com.br>
2023-02-18 21:08:52 -0300

    feat: add header component and its styles

---
+++ src/components/header/header.component.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import { HeaderContainer } from './header.styles'
+
+export const HeaderComponent: React.FC = () => {
+  return <HeaderContainer>Header</HeaderContainer>
+}
---
+++ src/components/header/header.styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.div`
+  grid-area: HD;
+  background-color: #252a48;
+`

1da552260b43c152b148598eed4e650273cd9bde
Vitor Souza <vitor.souza407@yahoo.com.br>
2023-02-18 21:09:30 -0300

    feat: add  GlobalStyles

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  * {
+   margin: 0;
+   padding: 0;
+   box-sizing: border-box;
+  }
+  
+  html, border-style, #root {
+    height:100%;
+  }
+  
+  *, button, input {
+    font-family: 'Roboto', sans-serif;
+    border: 0;
+    outline: 0;
+  }
+
+  button {
+    cursor: pointer;
+
+  }
+`

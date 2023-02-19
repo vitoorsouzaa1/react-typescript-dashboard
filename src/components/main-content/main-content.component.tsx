2bccda53b4c52a2d76261664617d582ad5576ba6
Vitor Souza <vitor.souza407@yahoo.com.br>
2023-02-18 21:09:47 -0300

    feat: add main-content component and its styles

---
+++ src/components/main-content/main-content.component.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import { MainContentContainer } from './main-content.styles'
+
+export const MainContentComponent: React.FC = () => {
+  return <MainContentContainer>MainContent</MainContentContainer>
+}
---
+++ src/components/main-content/main-content.styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const MainContentContainer = styled.div`
+  grid-area: MC;
+  background-color: #1b1f38;
+`

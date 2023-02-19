e5f434afb9d5d8ca007ade5fcd2b4b636ac1b71b
Vitor Souza <vitor.souza407@yahoo.com.br>
2023-02-18 21:09:15 -0300

    feat: add sidebar component and its styles

---
+++ src/components/sidebar/sidebar.component.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+// Styles
+import { SidebarContainer } from './sidebar.styles'
+
+export const SidebarComponent: React.FC = () => {
+  return <SidebarContainer>Sidebar</SidebarContainer>
+}
---
+++ src/components/sidebar/sidebar.styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const SidebarContainer = styled.div`
+  grid-area: SB;
+  background-color: #252a48;
+`

b83da1a5b17b37622c222d68c2233dffa5b4be13
Vitor Souza <vitor.souza407@yahoo.com.br>
2023-03-16 13:53:42 -0300

    feat: add SidebarComponent and its icons/styles

--- src/components/sidebar/sidebar.component.tsx
+++ src/components/sidebar/sidebar.component.tsx
@@ -1,8 +1,48 @@
 import React from 'react'
+import {
+  MdDashboard,
+  MdArrowDownward,
+  MdArrowUpward,
+  MdExitToApp,
+} from 'react-icons/md'
+
+// Images
+import logoImg from '../../assets/logo.svg'
 
 // Styles
-import { SidebarContainer } from './sidebar.styles'
+import {
+  SidebarContainer,
+  SidebarHeader,
+  SidebarMenu,
+  MenuItemLink,
+  LogImg,
+} from './sidebar.styles'
 
 export const SidebarComponent: React.FC = () => {
-  return <SidebarContainer>Sidebar</SidebarContainer>
+  return (
+    <SidebarContainer>
+      <SidebarHeader>
+        <LogImg src={logoImg} alt='Logo do Dashboard' />
+      </SidebarHeader>
+
+      <SidebarMenu>
+        <MenuItemLink href='#'>
+          <MdDashboard />
+          Dashboard
+        </MenuItemLink>
+        <MenuItemLink href='#'>
+          <MdArrowUpward />
+          Entradas
+        </MenuItemLink>
+        <MenuItemLink href='#'>
+          <MdArrowDownward />
+          Saidas
+        </MenuItemLink>
+        <MenuItemLink href='#'>
+          <MdExitToApp />
+          Sair
+        </MenuItemLink>
+      </SidebarMenu>
+    </SidebarContainer>
+  )
 }
--- src/components/sidebar/sidebar.styles.ts
+++ src/components/sidebar/sidebar.styles.ts
@@ -3,5 +3,45 @@ import styled from 'styled-components'
 export const SidebarContainer = styled.div`
   grid-area: SB;
   background-color: ${(props) => props.theme.colors.secondary};
+  padding-left: 20px;
+  border-right: 1px solid ${(props) => props.theme.colors.gray};
+`
+
+export const SidebarHeader = styled.header`
+  display: flex;
+  align-items: center;
+  height: 70px;
+`
+export const LogImg = styled.img`
+  height: 40px;
+  width: 40px;
+`
+
+export const SidebarImgTitle = styled.h3`
   color: ${(props) => props.theme.colors.white};
+  margin-left: 10px;
+`
+
+export const SidebarMenu = styled.nav`
+  margin-top: 50px;
+`
+
+export const MenuItemLink = styled.a`
+  color: ${(props) => props.theme.colors.info};
+  text-decoration: none;
+  flex-direction: column;
+  margin: 7px 0;
+  transition: opacity 0.3s;
+  display: flex;
+  align-items: center;
+
+  &:hover {
+    opacity: 0.7;
+    cursor: pointer;
+  }
+
+  > svg {
+    font-size: 18px;
+    margin-right: 5px;
+  }
 `

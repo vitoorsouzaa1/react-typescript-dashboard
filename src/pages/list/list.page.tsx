9b2e79c93003e266f894f6b33b48b0171ea5c855
Vitor Souza <vitor.souza407@yahoo.com.br>
2023-03-20 14:52:19 -0300

    feat: ensure ListPage is now dynamic and show correct lineColor and title, also add a ListContainer style element

--- src/pages/list/list.page.tsx
+++ src/pages/list/list.page.tsx
@@ -1,5 +1,25 @@
 import React from 'react'
 
+// Components
+import { ContentHeaderComponent } from '../../components/content-header/content-header.component'
+import { SelectIpuntComponent } from '../../components/select-input/select-ipunt.component'
+
+// Styles
+import { ListContainer } from './list.styles'
+
 export const ListPage: React.FC = () => {
-  return <h1>List</h1>
+  const options = [
+    {
+      value: 'Maria',
+      label: 'Maria',
+    },
+  ]
+
+  return (
+    <ListContainer>
+      <ContentHeaderComponent title='Saídas' lineColor='#e44c4e'>
+        <SelectIpuntComponent options={options} />
+      </ContentHeaderComponent>
+    </ListContainer>
+  )
 }

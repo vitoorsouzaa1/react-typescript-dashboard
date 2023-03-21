4d6e609c1c505f226452f72026b80de3a7ab06d9
Vitor Souza <vitor.souza407@yahoo.com.br>
2023-03-21 14:17:19 -0300

    style: change some styles to make the card and the text smoother

--- src/components/history-list/history-list.styles.ts
+++ src/components/history-list/history-list.styles.ts
@@ -6,7 +6,7 @@ interface ITagProps {
 export const HistoryListContainer = styled.li`
   background-color: ${(props) => props.theme.colors.tertiary};
   list-style: none;
-  border-radius: 5px;
+  border-radius: 10px;
   margin: 10px 0;
   padding: 12px 10px;
   display: flex;
@@ -27,10 +27,15 @@ export const HistoryListContainer = styled.li`
     justify-content: space-between;
     padding-left: 10px;
   }
+
+  > div span {
+    font-weight: 500;
+    font-size: 20px;
+  }
 `
 
 export const Tag = styled.div<ITagProps>`
-  width: 10px;
+  width: 13px;
   height: 60%;
   background-color: ${(props) => props.color};
   position: absolute;

c01c2ec4d02392dba8266529f789c7c3c2bd4a28
Vitor Souza <vitor.souza407@yahoo.com.br>
2023-03-25 18:35:09 -0300

    feat: add PieChartComponent and it's styles

---
+++ src/components/piechart/piechart.component.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts'
+
+// Styles
+import {
+  PieChartContainer,
+  SideContainer,
+  Subtexts,
+  SubtextContainer,
+  GraphContainer,
+} from './piechart.styles'
+
+interface IPieChartComponentProps {
+  data: {
+    name: string
+    value: number
+    percent: number
+    color: string
+  }[]
+}
+
+export const PieChartComponent: React.FC<IPieChartComponentProps> = ({
+  data,
+}) => (
+  <PieChartContainer>
+    <SideContainer>
+      <h2>Relação</h2>
+      <SubtextContainer>
+        {data.map((indicator) => (
+          <Subtexts key={indicator.name} color={indicator.color}>
+            <div>{indicator.percent}</div>
+            <span>{indicator.name}</span>
+          </Subtexts>
+        ))}
+      </SubtextContainer>
+    </SideContainer>
+
+    <GraphContainer>
+      <ResponsiveContainer>
+        <PieChart>
+          <Pie data={data} dataKey='percent'>
+            {data.map((indicator) => (
+              <Cell key={indicator.name} fill={indicator.color} />
+            ))}
+          </Pie>
+        </PieChart>
+      </ResponsiveContainer>
+    </GraphContainer>
+  </PieChartContainer>
+)
---
+++ src/components/piechart/piechart.styles.ts
@@ -0,0 +1,69 @@
+import styled from 'styled-components'
+
+interface ISubtextProps {
+  color: string
+}
+
+export const PieChartContainer = styled.div`
+  width: 48%;
+  height: 260px;
+  margin: 10px 0;
+  background-color: ${(props) => props.theme.colors.tertiary};
+  color: ${(props) => props.theme.colors.white};
+  border-radius: 7px;
+  display: flex;
+`
+
+export const SideContainer = styled.aside`
+  padding: 30px 20px;
+
+  > h2 {
+    margin-bottom: 60px;
+  }
+`
+
+export const SubtextContainer = styled.ul`
+  list-style: none;
+  height: 175px;
+  overflow-y: auto;
+  padding-right: 15px;
+
+  ::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: ${(props) => props.theme.colors.secondary};
+    border-radius: 10px;
+  }
+
+  ::-webkit-scrollbar-track {
+    background-color: ${(props) => props.theme.colors.tertiary};
+  }
+`
+
+export const Subtexts = styled.li<ISubtextProps>`
+  display: flex;
+  align-items: center;
+  margin-bottom: 7px;
+
+  > div {
+    background-color: ${(props) => props.color};
+    width: 40px;
+    height: 40px;
+    border-radius: 5px;
+    font-size: 18px;
+    line-height: 40px;
+    text-align: center;
+  }
+
+  > span {
+    margin-left: 5px;
+  }
+`
+
+export const GraphContainer = styled.main`
+  display: flex;
+  flex: 1;
+  justify-content: center;
+`

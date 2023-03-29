58b39f5f62594d266b6e8e80d2af27bf33e252aa
Vítor Souza <vitor.souza407@yahoo.com.br>
2023-03-29 19:27:00 -0300

    feat: add AuthProvider

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom/client'
 
 // Custom Hooks
 import { ThemeProvider } from './hooks/useTheme'
+import { AuthProvider } from './hooks/useAuth'
 
 // Components
 import { App } from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
 root.render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 )

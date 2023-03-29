18f9aa2ae2a9f475fcf1a85a9dfc0505a5557f7f
Vítor Souza <vitor.souza407@yahoo.com.br>
2023-03-29 19:25:34 -0300

    refactor: change routes for more legibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 // Custom Hooks
 import { useTheme } from './hooks/useTheme'
 
-// Components
-import { LayoutComponent } from './components/layout/layout.component'
-
-// Pages
-import { ListPage } from './pages/list/list.page'
-import { DashboardPage } from './pages/dashboard/dashboard.page'
+// Routes
+import { Routes } from './routes/routes.router'
 
 // Styles
 import GlobalStyles from './styles/GlobalStyles'
@@ -19,17 +14,9 @@ export const App: React.FC = () => {
   const { theme } = useTheme()
 
   return (
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        <LayoutComponent>
-          <Routes>
-            <Route path='/' element={<DashboardPage />} />
-            <Route path='/dashboard' element={<DashboardPage />} />
-            <Route path='/list/:type' element={<ListPage />} />
-          </Routes>
-        </LayoutComponent>
-      </ThemeProvider>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Routes />
+    </ThemeProvider>
   )
 }

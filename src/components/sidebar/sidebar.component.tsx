9af6386e781232f8a7d2f12f9bbf217b274563c3
Vítor Souza <vitor.souza407@yahoo.com.br>
2023-03-30 16:41:33 -0300

    feat: add a ToggleMenuComponent to show theme changer

--- src/components/sidebar/sidebar.component.tsx
+++ src/components/sidebar/sidebar.component.tsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   MdDashboard,
   MdArrowDownward,
   MdArrowUpward,
   MdExitToApp,
+  MdClose,
+  MdMenu,
 } from 'react-icons/md'
+
+// Components
+import { TogglerComponent } from '../toggle/toggler.component'
 
 // Images
 import logoImg from '../../assets/logo.svg'
 
 // Custom Hooks
 import { useAuth } from '../../hooks/useAuth'
+import { useTheme } from '../../hooks/useTheme'
 
 // Styles
 import {
@@ -21,14 +27,35 @@ import {
   LogImg,
   SidebarImgTitle,
   MenuItemButton,
+  ThemeToggleFooter,
+  ToggleMenu,
 } from './sidebar.styles'
 
 export const SidebarComponent: React.FC = () => {
   const { signOut } = useAuth()
+  const { toggleTheme, theme } = useTheme()
+
+  const [toggleMenuIsOpened, setToggleMenuIsOpened] = useState(false)
+  const [darkTheme, setDarkTheme] = useState(() =>
+    theme.title === 'dark' ? true : false
+  )
+
+  const handleToggleMenu = () => {
+    setToggleMenuIsOpened(!toggleMenuIsOpened)
+  }
+
+  const handleChangeTheme = () => {
+    setDarkTheme(!darkTheme)
+    toggleTheme()
+  }
 
   return (
-    <SidebarContainer>
+    <SidebarContainer menuIsOpen={toggleMenuIsOpened}>
       <SidebarHeader>
+        <ToggleMenu onClick={handleToggleMenu}>
+          {toggleMenuIsOpened ? <MdClose /> : <MdMenu />}
+        </ToggleMenu>
+
         <LogImg src={logoImg} alt='Logo do Dashboard' />
         <SidebarImgTitle>FinanceBoard</SidebarImgTitle>
       </SidebarHeader>
@@ -54,6 +81,15 @@ export const SidebarComponent: React.FC = () => {
           Sair
         </MenuItemButton>
       </SidebarMenu>
+
+      <ThemeToggleFooter menuIsOpen={toggleMenuIsOpened}>
+        <TogglerComponent
+          labelLeft='Light'
+          labelRight='Dark'
+          checked={darkTheme}
+          onChange={handleChangeTheme}
+        />
+      </ThemeToggleFooter>
     </SidebarContainer>
   )
 }

7b85cd04e8574ce912b03ccb49192897df00bf02
Vítor Souza <vitor.souza407@yahoo.com.br>
2023-03-30 16:41:19 -0300

    feat: add a ToggleMenuComponent to show theme changer; style: add responsiveness for the SidebarComponent

--- src/components/sidebar/sidebar.styles.ts
+++ src/components/sidebar/sidebar.styles.ts
@@ -1,10 +1,33 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-export const SidebarContainer = styled.div`
+interface ISidebarContainerProps {
+  menuIsOpen: boolean
+}
+
+interface IThemeTogglerFooterProps {
+  menuIsOpen: boolean
+}
+
+export const SidebarContainer = styled.div<ISidebarContainerProps>`
   grid-area: SB;
   background-color: ${(props) => props.theme.colors.secondary};
   padding-left: 20px;
   border-right: 1px solid ${(props) => props.theme.colors.gray};
+
+  @media (max-width: 600px) {
+    padding-left: 20px;
+    position: fixed;
+    z-index: 2;
+    width: 170px;
+    height: ${(props) => (props.menuIsOpen ? '100vh' : '70px')};
+    overflow: hidden;
+    ${(props) =>
+      !props.menuIsOpen &&
+      css`
+        border: none;
+        border-bottom: 1px solid ${(props) => props.theme.colors.gray};
+      `};
+  }
 `
 
 export const SidebarHeader = styled.header`
@@ -15,11 +38,19 @@ export const SidebarHeader = styled.header`
 export const LogImg = styled.img`
   height: 40px;
   width: 40px;
+
+  @media (max-width: 600px) {
+    display: none;
+  }
 `
 
 export const SidebarImgTitle = styled.h3`
   color: ${(props) => props.theme.colors.white};
   margin-left: 10px;
+
+  @media (max-width: 600px) {
+    display: none;
+  }
 `
 
 export const SidebarMenu = styled.nav`
@@ -64,5 +95,36 @@ export const MenuItemButton = styled.button`
   > svg {
     font-size: 18px;
     margin-right: 5px;
+  }
+`
+
+export const ToggleMenu = styled.button`
+  width: 40px;
+  height: 40px;
+  border-radius: 5px;
+  font-size: 22px;
+  background-color: ${(props) => props.theme.colors.warning};
+  color: ${(props) => props.theme.colors.white};
+  transition: opacity 0.3s;
+  display: none;
+
+  &:hover {
+    opacity: 0.7;
+  }
+
+  @media (max-width: 600px) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`
+
+export const ThemeToggleFooter = styled.footer<IThemeTogglerFooterProps>`
+  display: none;
+  position: absolute;
+  bottom: 30px;
+
+  @media (max-width: 470px) {
+    display: ${(props) => (props.menuIsOpen ? 'flex' : 'none')};
   }
 `
